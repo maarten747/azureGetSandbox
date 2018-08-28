849ff1f606b6e14bf6d605fea3ce9b81933b3e19
Sandbox Sync <support@getsandbox.com>
2018-08-28 16:51:01 +0000

    Changes

--- stubs/main.js
+++ stubs/main.js
@@ -26,12 +26,6 @@ Sandbox.define('/orders/simulations', 'POST', function(req, res){
     
     console.log('MDS test console');
     
-    //for(i=0; i<2147483000; i++){
-    for(i=0; i<8147483; i++){
-        
-    }
-    
-    //return window.setTimeout(res.json( orderSimulate.createHeaderOrderJson(req.body) ), 5000);
     return res.json( orderSimulate.createHeaderOrderJson(req.body) );
 });
 
@@ -39,6 +33,12 @@ Sandbox.define('/orders/simulations', 'POST', function(req, res){
 Sandbox.define('/orders', 'POST', function(req, res){
     
     console.log('MDS test console');
+    
+    // 5 seconds
+    for(i=0; i<14147483; i++){
+        
+    }
+    
     return res.json( orderSimulate.createHeaderOrderJson(req.body) );
 });
  
