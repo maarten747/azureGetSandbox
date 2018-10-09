22a03a41470be644ccd84f517d742f8dcb8c21e8
Sandbox Sync <support@getsandbox.com>
2018-10-09 12:55:38 +0000

    Changes

--- stubs/productInfoCalls.js
+++ stubs/productInfoCalls.js
@@ -7,7 +7,7 @@ exports.searchStock = function  searchStock(requestBody){
     requestBody.forEach(function(sku){
         var dataElement = {
                             Product: generateAzureProduct(sku),
-                            Quantity: 20,
+                            Quantity: 16,
                             Arrivals:[
                                         {
                                             "Date": "2018-11-02T00:00:00",
